afd846946f182919c00c4ae77481341b86f95873
Ribes Roberto <rbo@tgw-group.com>
2019-06-27 12:28:22 +0200

    First Draft of Hydraulic app

---
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.Hydraulics/CanalSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScienceAndMaths.Hydraulics
+{
+    public abstract class CanalSection
+    {
+        public double HydraulicRadius
+        {
+            get { return HydraulicArea() / HydraulicPerimeter(); }
+        }
+
+        public abstract double HydraulicArea();
+
+        public abstract double HydraulicPerimeter();
+        
+    }
+}
--- products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.Service/Program.cs
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.Service/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Rob.Engineering.ScienceAndMaths.Service
+﻿using System;
+
+namespace Rob.Engineering.ScienceAndMaths.Service
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write(Environment.NewLine + Environment.NewLine + "--- Science And Maths System Startup ---" + Environment.NewLine);
+            Con
         }
     }
 }
