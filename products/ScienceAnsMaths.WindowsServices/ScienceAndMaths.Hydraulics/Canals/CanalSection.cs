8b7c76013ae7c9f20cacff23ccd0036e4cb8305a
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-04-13 12:20:33 +0200

    Adapted formulas

---
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.Hydraulics/Canals/CanalSection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ScienceAndMaths.Shared;
+
+namespace ScienceAndMaths.Hydraulics.Canals
+{
+    /// <summary>
+    /// This class describes an uniform canal section of a given length an shape
+    /// </summary>
+    public abstract class CanalSection
+    {
+        /// <summary>
+        /// Roughness coefficient applyin Manning's rule
+        /// </summary>
+        public double Roughness { get; set; }
+
+        /// <summary>
+        /// Length of the canal section
+        /// </summary>
+        public double Length { get; set; }
+        
+        /// <summary>
+        /// Slope of the canal section in 1/1 (m/m)
+        /// </summary>
+        public double Slope { get; set; }
+
+        /// <summary>
+        /// Returns the Froude number of a canal section at a given length
+        /// </summary>
+        /// <param name="waterLevel">The level of the water in the canal section</param>
+        /// <returns></returns>
+        public double GetFroudeNumber(double waterLevel)
+        {
+            return GetManningVelocity(waterLevel) / Math.Sqrt(Constants.GravityAcceleration * GetHydraulicDepth(waterLevel));
+        }
+
+        /// <summary>
+        /// Returns the speed of the water at a given point by means of the Manning formula
+        /// </summary>
+        /// <param name="waterLevel">The level of the water in the canal section</param>
+        /// <returns></returns>
+        public double GetManningVelocity(double waterLevel)
+        {
+            return (1 / Slope) * Math.Pow(GetHydraulicRadius(waterLevel), 2.0/3.0) * Math.Sqrt(Slope);
+        }
+
+        /// <summary>
+        /// Returns the hydraulic radius
+        /// </summary>
+        /// <param name="waterLevel">The level of the water in the canal section</param>
+        /// <returns></returns>
+        public double GetHydraulicRadius(double waterLevel)
+        {
+            return GetHydraulicArea(waterLevel) / GetHydraulicPerimeter(waterLevel); 
+        }
+
+        /// <summary>
+        /// Returns the Hydraulic depth that can be used to obtain the Froude number
+        /// </summary>
+        /// <param name="waterLevel">The level of the water in the canal section</param>
+        /// <returns></returns>
+        public abstract double GetHydraulicDepth(double waterLevel);
+
+        /// <summary>
+        /// Returns the Hydraulic area of the canal section
+        /// </summary>
+        /// <param name="waterLevel">The level of the water in the canal section</param>
+        /// <returns></returns>
+        public abstract double GetHydraulicArea(double waterLevel);
+
+        /// <summary>
+        /// Returns the Hydraulic perimeter of the canal section
+        /// </summary>
+        /// <param name="waterLevel">The level of the water in the canal section</param>
+        /// <returns></returns>
+        public abstract double GetHydraulicPerimeter(double waterLevel);
+ 
+    }
+}
---
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.Hydraulics/Canals/RectangularSection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScienceAndMaths.Hydraulics.Canals
+{
+    public class RectangularSection : CanalSection
+    {
+        public double Width { get; set; }
+
+        public override double GetHydraulicDepth(double waterLevel)
+        {
+            return waterLevel;
+        }
+
+        public override double GetHydraulicArea(double waterLevel)
+        {
+            return waterLevel * Width;
+        }
+
+        public override double GetHydraulicPerimeter(double waterLevel)
+        {
+            return Width + 2 * waterLevel;
+        }
+    }
+}
