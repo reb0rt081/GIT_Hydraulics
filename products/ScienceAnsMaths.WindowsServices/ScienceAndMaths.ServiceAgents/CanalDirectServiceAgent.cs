089f684b4691b7a77cd98e030f1f999c99b8de27
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-05-01 10:37:10 +0200

    Service agent created

---
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.ServiceAgents/CanalDirectServiceAgent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScienceAndMaths.Domain;
+using ScienceAndMaths.Shared.Canals;
+using Unity;
+
+namespace ScienceAndMaths.ServiceAgents
+{
+    public class CanalDirectServiceAgent : ICanalServiceAgent
+    {
+        [Dependency]
+        public ICanalFlowService CanalFlowService { get; set; }
+
+        public CanalSimulationResult ExecuteCanalSimulation()
+        {
+            // Normally use correlation Ids to correlate messages
+            return CanalFlowService.ExecuteCanalSimulation();
+        }
+    }
+}
---
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.ServiceAgents/ICanalServiceAgent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScienceAndMaths.Shared.Canals;
+
+namespace ScienceAndMaths.ServiceAgents
+{
+    public interface ICanalServiceAgent
+    {
+        CanalSimulationResult ExecuteCanalSimulation();
+    }
+}
