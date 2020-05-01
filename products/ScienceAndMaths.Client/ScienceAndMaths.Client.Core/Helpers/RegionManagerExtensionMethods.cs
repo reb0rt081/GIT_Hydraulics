5dbd67e8cf911c787989fe253da1175c69491f63
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-05-01 17:37:48 +0200

    More comments

--- products/ScienceAndMaths.Client/ScienceAndMaths.Client.Core/Helpers/RegionManagerExtensionMethods.cs
+++ products/ScienceAndMaths.Client/ScienceAndMaths.Client.Core/Helpers/RegionManagerExtensionMethods.cs
@@ -9,7 +9,8 @@ using ScienceAndMaths.Client.Shared;
 namespace ScienceAndMaths.Client.Core.Helpers
 {
     /// <summary>
-    /// Example of how we could override the regionmanager to allow navigation to ribbons. Bear in mind the ideal solution is to do this overriding the navigation service
+    /// Example of how we could override the regionmanager to allow navigation to ribbons. Bear in mind the ideal solution is to do this overriding the navigation service.
+    /// There are methods in PrismApplicationBase and PrismApplication that can be overriden to replace the region manager by a custom one where this could be achieved easily.
     /// </summary>
     public static class RegionManagerExtensionMethods
     {
