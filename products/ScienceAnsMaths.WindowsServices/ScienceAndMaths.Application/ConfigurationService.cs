4c05a1f5e7ddf143a2ae0a2d6e24a9d5ceeaf1ff
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-05-02 18:05:12 +0200

    ConfigurationService

---
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.Application/ConfigurationService.cs
@@ -0,0 +1,20 @@
+﻿using ScienceAndMaths.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace ScienceAndMaths.Application
+{
+    public class ConfigurationService : IConfigurationService
+    {
+        [Dependency]
+        public ICanalConfigurationLoader ConfigurationLoader { get; set; }
+        public void LoadCanalConfiguration(string file)
+        {
+            ConfigurationLoader.LoadCanalConfiguration(file);
+        }
+    }
+}
