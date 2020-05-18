1f98590a41b8c10c1b345ed2cf36fad821168d14
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-05-18 21:35:47 +0200

    Improved display

--- products/ScienceAndMaths.Client/ScienceAndMaths.Client.Modules/Canal/ViewModels/CanalViewModel.cs
+++ products/ScienceAndMaths.Client/ScienceAndMaths.Client.Modules/Canal/ViewModels/CanalViewModel.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using ScienceAndMaths.Client.Modules.Canal.Views;
-
+﻿using System.Threading.Tasks;
 using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Regions;
 using ScienceAndMaths.Client.Shared;
 using ScienceAndMaths.Shared.Canals;
--- products/ScienceAndMaths.Client/ScienceAndMaths.Client.Modules/Canal/Views/CanalView.xaml.cs
+++ products/ScienceAndMaths.Client/ScienceAndMaths.Client.Modules/Canal/Views/CanalView.xaml.cs
@@ -1,22 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 using ScienceAndMaths.Client.Modules.Canal.ViewModels;
 using ScienceAndMaths.Shared.Canals;
-
 using Unity;
 
 namespace ScienceAndMaths.Client.Modules.Canal.Views
@@ -87,7 +74,6 @@ namespace ScienceAndMaths.Client.Modules.Canal.Views
                     canalCanvas.Children.Add(canalLine);
 
                     previousX1 = canalLine.X2;
-                    previousY1 = canalLine.Y2;
                 }
             }
 
