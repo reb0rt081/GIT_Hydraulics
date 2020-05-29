bd20cd6fd585a9afea64c3958daa4d469d659cac
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-05-29 19:43:53 +0200

    New test for canal with slope

--- products/ScienceAnsMaths.WindowsServices/Test.U/ScienceAndMaths.Hydraulics.Test/CanalTest.cs
+++ products/ScienceAnsMaths.WindowsServices/Test.U/ScienceAndMaths.Hydraulics.Test/CanalTest.cs
@@ -29,5 +29,28 @@ namespace ScienceAndMaths.Hydraulics.Test
             Assert.IsTrue(fr2 - 0.0690 <= 0.0001);
             Assert.IsTrue(result - 2.89888 <= 0.00001);
         }
+
+        [TestMethod]
+        public void BasicRectangularFlowTest2()
+        {
+            CanalStretch canal = new CanalStretch(602, 10, new RectangularSection(4, 0.014, 0.0004));
+
+            double rh = canal.CanalSection.GetHydraulicRadius(0.694);
+            double mv = canal.CanalSection.GetManningVelocity(0.694);
+            double fr2 = Math.Pow(canal.GetFroudeNumber(0.694), 2.0);
+
+            double flow = canal.CanalSection.GetManningFlow(0.694);
+
+            RungeKutta solver = new RungeKutta(1, canal.FlowEquation());
+
+            //  Act
+            double result = solver.Solve(0, 0.694);
+
+            double actualRh = (4.0 * 0.694) / (4.0 + 2.0 * 0.694);
+            Assert.IsTrue(rh - actualRh <= double.Epsilon);
+            Assert.IsTrue(mv - 0.91811 <= 0.0001);
+            Assert.IsTrue(fr2 - 1.90669 <= 0.0001);
+            Assert.IsTrue(result - 0.700444 <= 0.00001);
+        }
     }
 }
