48af6e231a0abf96f6651d072dc5f8813a0fc8ae
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-06-03 15:04:29 +0200

    NewtonRaphson method implemented

---
+++ products/ScienceAnsMaths.WindowsServices/ScienceAndMaths.Mathematics/NewtonRaphson.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScienceAndMaths.Mathematics
+{
+    /// <summary>
+    /// Represents a class to solve non-linear equations in one variable: 0 = f(x)
+    /// </summary>
+    public class NewtonRaphson
+    {
+        public NewtonRaphson(Func<double, double> function, Func<double, double> derivativeFunction = null)
+        {
+            Function = function;
+
+            DerivativeFunction = derivativeFunction;
+        }
+
+        public Func<double, double> Function { get; set; }
+
+        public Func<double, double> DerivativeFunction { get; set; }
+
+        public double Solve(double initialValue, double epsilon)
+        {
+            double x = initialValue;
+            
+            double h = Function(x) / GetDerivativeValue(x, epsilon);
+            while (Math.Abs(h) >= epsilon)
+            {
+                h = Function(x) / GetDerivativeValue(x, epsilon);
+
+                // x(i+1) = x(i) - f(x) / f'(x)   
+                x = x - h;
+            }
+
+            return x;
+        }
+
+        private double GetDerivativeValue(double x, double epsilon)
+        {
+            double derivativeValue;
+
+            if (DerivativeFunction == null)
+            {
+                derivativeValue = (Function(x + epsilon) - Function(x)) / epsilon;
+            }
+            else
+            {
+                derivativeValue = DerivativeFunction(x);
+            }
+
+            return derivativeValue;
+        }
+    }
+}
---
+++ products/ScienceAnsMaths.WindowsServices/Test.U/ScienceAndMaths.Mathematics.Test/NewtonRaphsonTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScienceAndMaths.Mathematics.Test
+{
+    [TestClass]
+    public class NewtonRaphsonTest
+    {
+        [TestMethod]
+        public void BasicEquationSolve()
+        {
+            double epsilon = 0.0001;
+            NewtonRaphson newtonRaphson = new NewtonRaphson((x => x + 5), x => 1);
+
+            double result = newtonRaphson.Solve(0, epsilon);
+
+            Assert.IsTrue(Math.Abs(-5 - result) < epsilon);
+        }
+
+        [TestMethod]
+        public void BasicEquationNoDerivativeSolve()
+        {
+            double epsilon = 0.0001;
+            NewtonRaphson newtonRaphson = new NewtonRaphson((x => x + 5));
+
+            double result = newtonRaphson.Solve(0, epsilon);
+
+            Assert.IsTrue(Math.Abs(-5 - result) < epsilon);
+        }
+
+        [TestMethod]
+        public void SecondDegreeEquationNoDerivativeSolve()
+        {
+            double epsilon = 0.0001;
+            NewtonRaphson newtonRaphson = new NewtonRaphson((x => x * x + 2 * x + 1));
+
+            double result = newtonRaphson.Solve(0, epsilon);
+
+            double error = Math.Abs(-1 - result);
+            Assert.IsTrue(error <= epsilon * 10);
+        }
+
+        /// <summary>
+        /// Finds closes value
+        /// </summary>
+        [TestMethod]
+        public void SecondDegreeEquationNoDerivativeSolve2()
+        {
+            double epsilon = 0.0001;
+            NewtonRaphson newtonRaphson = new NewtonRaphson((x => x * x - x -2));
+
+            double result = newtonRaphson.Solve(10, epsilon);
+
+            double error = Math.Abs(2 - result);
+            Assert.IsTrue(error <= epsilon * 10);
+        }
+    }
+}
