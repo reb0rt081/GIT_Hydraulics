c318263461bda6159541b15e9dca0de18a59af19
Roberto Ribes <roberto.ribes@tgw-group.com>
2020-08-18 15:28:59 +0200

    Composite canal added

--- products/ScienceAnsMaths.WindowsServices/Test.U/ScienceAndMaths.Configuration.Test/CanalConfigurationTest.cs
+++ products/ScienceAnsMaths.WindowsServices/Test.U/ScienceAndMaths.Configuration.Test/CanalConfigurationTest.cs
@@ -88,6 +88,99 @@ namespace ScienceAndMaths.Configuration.Test
         }
 
         [TestMethod]
+        public void CombinedRectangularSerializingTest()
+        {
+            //  Arrange
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(CanalConfiguration));
+            CanalConfiguration canalConfiguration = new CanalConfiguration();
+            canalConfiguration.ConfigId = "TestConfig";
+
+            CanalNode initNode = new CanalNode()
+            {
+                NodeId = "InitNode"
+            };
+            CanalNode midNode = new CanalNode()
+            {
+                NodeId = "MiddleNode"
+            };
+            CanalNode endNode = new CanalNode()
+            {
+                NodeId = "EndNode"
+            };
+            CanalArrow arrow1 = new CanalArrow()
+            {
+                ArrowId = "Arrow1",
+                Length = 600.0,
+                Flow = 2.5486840725260249,
+                FromNodeId = initNode.NodeId,
+                ToNodeId = midNode.NodeId
+            };
+            arrow1.CanalSection = new RectangularSectionConfiguration()
+            {
+                Roughness = 0.014,
+                Slope = 0.0004,
+                Width = 4.0
+            };
+
+            CanalArrow arrow2 = new CanalArrow()
+            {
+                ArrowId = "Arrow2",
+                Length = 600.0,
+                Flow = 2.5486840725260249,
+                FromNodeId = midNode.NodeId,
+                ToNodeId = endNode.NodeId
+            };
+            arrow2.CanalSection = new RectangularSectionConfiguration()
+            {
+                Roughness = 0.014,
+                Slope = 0.004,
+                Width = 4.0
+            };
+
+            canalConfiguration.Arrows = new List<CanalArrow>() { arrow1, arrow2 };
+            canalConfiguration.Nodes = new List<CanalNode>() { initNode, midNode, endNode };
+            var xml = "";
+
+            //  Act
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    xmlSerializer.Serialize(writer, canalConfiguration);
+                    xml = sww.ToString(); // Your XML
+                }
+            }
+
+            //  Assert
+            Assert.IsTrue(xml != null);
+
+            CanalConfiguration deserializedConfiguration = null;
+
+            using (StringReader sr = new StringReader(xml))
+            {
+                deserializedConfiguration = (CanalConfiguration)xmlSerializer.Deserialize(sr);
+            }
+
+            Assert.IsTrue(deserializedConfiguration != null);
+            Assert.AreEqual(3, deserializedConfiguration.Nodes.Count);
+            Assert.AreEqual(2, deserializedConfiguration.Arrows.Count);
+
+            CanalConfigurationConverter converter = new CanalConfigurationConverter();
+
+            var data = converter.Convert(deserializedConfiguration);
+
+            Assert.IsTrue(data != null);
+            Assert.AreEqual(3, data.CanelEdges.Count);
+            Assert.AreEqual(2, data.CanalStretches.Count);
+
+            canalConfiguration = converter.ConvertBack(data);
+
+            Assert.IsTrue(canalConfiguration != null);
+            Assert.AreEqual(3, canalConfiguration.Nodes.Count);
+            Assert.AreEqual(2, canalConfiguration.Arrows.Count);
+        }
+
+        [TestMethod]
         public void RectangularWithSluidgeGateSerializingTest()
         {
             //  Arrange
