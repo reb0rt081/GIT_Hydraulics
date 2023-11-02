0af0a125db9ddcdf0d01615e0070a65e1ae24454
Roberto Ribes <rbo@tgw-group.com>
2023-11-02 13:52:56 +0100

    Created csv file converter

---
+++ products/ScienceAndMaths.Libraries/ScienceAndMaths.MachineLearning/Tools/CsvFileConverter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ScienceAndMaths.MachineLearning.Tools
+{
+    public static class CsvFileConverter
+    {
+        /// <summary>
+        /// Converts an input CSV file into a new CSV file with only certain columns
+        /// </summary>
+        /// <param name="inputFile">@"C:\test.csv"</param>
+        /// <param name="outputFile"></param>
+        /// <param name="columnsToCopy"></param>
+        /// <param name="separator"></param>
+        public static void ConvertCsvToCsv(string inputFile, string outputFile, List<int> columnsToCopy, char separator = ',')
+        {
+            Dictionary<int, List<string>> entryTable = new Dictionary<int, List<string>>();
+            foreach (int columnId in columnsToCopy)
+            {
+                entryTable.Add(columnId, new List<string>());
+            }
+
+            using (var reader = new StreamReader(inputFile))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(separator);
+
+                    foreach (KeyValuePair<int, List<string>> entry in entryTable)
+                    {
+                        entry.Value.Add(values[entry.Key]);
+                    }
+                }
+            }
+
+            var csv = new StringBuilder();
+            int length = entryTable[0].Count;
+            for (int i = 0; i < length; i++)
+            {
+                string newLine = "";
+                bool firstInRowElement = true;
+                foreach (KeyValuePair<int, List<string>> entry in entryTable)
+                {
+                    
+                    newLine += firstInRowElement ? entry.Value[i] : $",{entry.Value[i]}";
+                    firstInRowElement = false;
+                }
+                csv.AppendLine(newLine);
+            }
+
+            File.WriteAllText(outputFile, csv.ToString());
+        }
+
+        public static void ConvertToCsv(string outputFile, string[][] data)
+        {
+            var csv = new StringBuilder();
+            foreach (string[] row in data)
+            {
+                string newLine = "";
+                bool firstInRowElement = true;
+                foreach (string cell in row)
+                {
+                    newLine += firstInRowElement ? cell : $",{cell}";
+                    firstInRowElement = false;
+                }
+                csv.AppendLine(newLine);
+            }
+
+            File.WriteAllText(outputFile, csv.ToString());
+        }
+    }
+}
---
+++ products/ScienceAndMaths.Libraries/Test.U/ScienceAndMaths.MachineLearning.Test/Tools/CsvFileConverterTest.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ScienceAndMaths.MachineLearning.Tools;
+
+namespace ScienceAndMaths.MachineLearning.Test.Tools
+{
+    [TestClass]
+    public class CsvFileConverterTest
+    {
+        [TestMethod]
+        public void CreateCsvFile()
+        {
+            string[][] data = new string[2][];
+            for (int i = 0; i < 2; ++i)
+                data[i] = new string[3];
+
+            data[0][0] = "column 1";
+            data[0][1] = "column 2";
+            data[0][2] = "column 3";
+
+            data[1][0] = "data row 1 column 1";
+            data[1][1] = "data row 1 column 2";
+            data[1][2] = "data row 1 column 3";
+
+            CsvFileConverter.ConvertToCsv(@"test.csv", data);
+            Assert.IsTrue(File.Exists("test.csv"));
+
+            CsvFileConverter.ConvertCsvToCsv("test.csv", "test2.csv", new List<int> {0, 2});
+            Assert.IsTrue(File.Exists("test2.csv"));
+
+            using (var reader = new StreamReader("test2.csv"))
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                
+                Assert.AreEqual(2, values.Length);
+            }
+        }
+    }
+}
